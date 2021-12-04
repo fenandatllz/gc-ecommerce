e936466bc3e56b084a569156fc20f83e2c57b10d
amezaconcordia <89050452+amezaconcordia@users.noreply.github.com>
2021-12-03 17:55:21 -0800

    Update crm.js
    prueba modificacion

--- functions/ecommerce/routes/crm.js
+++ functions/ecommerce/routes/crm.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const crm = require("../controllers/crm")
 
-
+console.log("Crm router")
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
 
